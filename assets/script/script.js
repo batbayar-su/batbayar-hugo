b9d4c5d2c185acdbb214cc50f76e1dc7e09ab0c6
Batbayar Sukhbaatar <batbayar.sukhbaatar@gmail.com>
2025-01-13 17:38:57 +0100

    remove service worker

--- assets/script/script.js
+++
@@ -1,9 +0,0 @@
-import params from '@params'
-
-if (params.environment === 'production') {
-  document.addEventListener('DOMContentLoaded', () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
-    }
-  })
-}
